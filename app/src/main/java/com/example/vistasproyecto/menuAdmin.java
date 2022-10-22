e27190dfa2a40dc0367ad7a866600e626664b9e6
LindaCastellanos05 <2021-100166@intecap.edu.gt>
2022-10-22 09:12:50 -0600

    pantalla menu admin conecta con boton ventas

--- app/src/main/java/com/example/vistasproyecto/menuAdmin.java
+++ app/src/main/java/com/example/vistasproyecto/menuAdmin.java
@@ -2,15 +2,27 @@ package com.example.vistasproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.vistasproyecto.R;
 
 public class menuAdmin extends AppCompatActivity {
-
+private Button btnverfacturas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_admin);
+
+        btnverfacturas = findViewById(R.id.btnventasadmin);
+
+        btnverfacturas.setOnClickListener(this::verFacturas);
+    }
+
+    private void verFacturas(View view) {
+        Intent intent = new Intent(menuAdmin.this, recyclerFactura.class);
+        startActivity(intent);
     }
 }
